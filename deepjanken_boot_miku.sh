a12f8f742b3b3a0737323d94875a0d4604b16071
karaage0703 <coldchicken@gmail.com>
2018-11-17 00:31:09 +0900

    Add miku voice demo

---
+++ deepjanken_boot_miku.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/deepjanken/deepjanken_ommf2018_miku.py --model_path ~/retrained_graph.binaryproto --label_path ~/retrained_labels.txt --input_height 160 --input_width 160 --input_layer input --output_layer final_result
